f068ad058aa6bcc657893873510510a3eb07ad98
aviral <avi14320@gmail.com>
2023-09-30 00:22:24 +0530

    initial commit

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -42,7 +42,7 @@ import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
 import Similar from "./carousels/Similar";
 import Recommendation from "./carousels/Recommendation";
-import PageNotFound from "../404/pageNotFound";
+import PageNotFound from "../404/PageNotFound";
 
 const Details = () => {
   const { mediaType, id } = useParams();
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { default as PageNotFound } from "./404/pageNotFound";
+import { default as PageNotFound } from "./404/PageNotFound";
 import { default as Home } from "./home/Home";
 import { default as Details } from "./details/Details";
 import { default as SearchResult } from "./searchResult/SearchResult";
